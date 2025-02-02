82beba0f164f9efab8c91a6d8cc2e42dc8f4cb4a
o.pereyra <onasis.pereyra@gmail.com>
2025-02-02 18:17:36 -0400

    Update Program.cs
    Changing variable names.

--- Homework 2 - Make a simple program/Simple program/Simple program.cs/Program.cs
+++ Homework 2 - Make a simple program/Simple program/Simple program.cs/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Ingrese un número para determinar si es par o impar:");
-var num = Console.ReadLine();
+var userNum = Console.ReadLine();
 
-var calc = Convert.ToInt32(num) % 2;
+var evenOrOddNumberCalc = Convert.ToInt32(userNum) % 2;
 
-if (calc == 0) {
+if (evenOrOddNumberCalc == 0) {
     Console.WriteLine("Es par.");
 } else {
     Console.WriteLine("Es impar.");
