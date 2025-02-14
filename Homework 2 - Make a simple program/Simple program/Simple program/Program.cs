46fd9efe9a21678a94ba9a720ff4c943ab66a894
o.pereyra <onasis.pereyra@gmail.com>
2025-02-14 18:13:29 -0400

    Update Program.cs
    Adding exception handling and making some adjustments to the variables.

--- Homework 2 - Make a simple program/Simple program/Simple program/Program.cs
+++ Homework 2 - Make a simple program/Simple program/Simple program/Program.cs
@@ -1,10 +1,29 @@
-﻿Console.WriteLine("Ingrese un número para determinar si es par o impar:");
-var userNum = Console.ReadLine();
+﻿try
+{
+    Console.WriteLine("Ingrese un número para determinar si es par o impar:");
+    int userNum = Convert.ToInt32(Console.ReadLine());
 
-var evenOrOddNumberCalc = Convert.ToInt32(userNum) % 2;
+    var evenOrOddNumberCalc = userNum % 2;
 
-if (evenOrOddNumberCalc == 0) {
-    Console.WriteLine("Es par.");
-} else {
-    Console.WriteLine("Es impar.");
+    if (evenOrOddNumberCalc == 0)
+    {
+        Console.WriteLine("Es par.");
+    }
+    else
+    {
+        Console.WriteLine("Es impar.");
+    }
 }
+
+catch (FormatException)
+{
+    Console.WriteLine("Ingresa solo números enteros por favor."); 
+}
+
+Console.WriteLine("Presiona una tecla para salir.");
+
+Console.ReadKey();
+
+
+
+
