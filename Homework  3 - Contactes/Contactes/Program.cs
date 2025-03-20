2980cf3e66c3ef9a86be79c18f57fc95526eb1e9
o.pereyra <onasis.pereyra@gmail.com>
2025-03-20 17:41:18 -0400

    Update Program.cs
    Creating functions with the methods already used to apply them in the switch structure.

--- Homework  3 - Contactes/Contactes/Program.cs
+++ Homework  3 - Contactes/Contactes/Program.cs
@@ -13,201 +13,50 @@ Dictionary<int, string> telephones = new Dictionary<int, string>();
 Dictionary<int, string> emails = new Dictionary<int, string>();
 Dictionary<int, int> ages = new Dictionary<int, int>();
 Dictionary<int, bool> bestFriends = new Dictionary<int, bool>();
-
+string getName = string.Empty;
+string getLastName = string.Empty;
+bool contactFound = false;
 
 try 
 {
-
+   
     while (runing)
     {
         Console.WriteLine(@"1. Agregar Contacto     2. Ver Contactos    3. Buscar Contactos     4. Modificar Contacto   5. Eliminar Contacto    6. Salir");
         Console.WriteLine("Digite el número de la opción deseada");
 
         int typeOption = Convert.ToInt32(Console.ReadLine());
-
-        string getName = string.Empty;
-        string getLastName = string.Empty;
-        var varNameAndLastNameIncorrect = string.Empty;
-        bool contactFound = false;
+        
 
         switch (typeOption)
         {
             case 1:
                 {
                     AddContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
-
                 }
                 break;
             case 2: //extract this to a method
                 {
-                    Console.WriteLine($"Nombre          Apellido            Dirección           Telefono            Email           Edad            Es Mejor Amigo?");
-                    Console.WriteLine($"____________________________________________________________________________________________________________________________");
-                    foreach (var id in ids)
-                    {
-                        var isBestFriend = bestFriends[id];                
-
-                        string isBestFriendStr = (isBestFriend == true) ? "Si" : "No";
-                        Console.WriteLine($"{names[id]}         {lastnames[id]}         {addresses[id]}         {telephones[id]}            {emails[id]}            {ages[id]}          {isBestFriendStr}");
-                    }
-
+                    ViewContacts(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
                 }
                 break;
             case 3: //search
                 {
-                 
-                    Console.WriteLine("Ingrese el nombre del contacto que desea buscar: ");
-                    getName = Console.ReadLine().ToLower();
-
-                    Console.WriteLine("Ingrese el apellido del contacto que desea buscar: ");
-                    getLastName = Console.ReadLine().ToLower();
-
-
-                    foreach (var id in ids)
-                    {
-
-                            if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
-                            {
-                            
-                            Console.WriteLine($"""
-                            Datos del contacto:
-                            Nombre: {names[id]}
-                            Apellido: {lastnames[id]}
-                            Dirección: {addresses[id]}
-                            Teléfono: {telephones[id]}
-                            Email: {emails[id]}
-                            Edad: {ages[id]}
-                            Es mejor amigo?: {bestFriends[id]}
-                            """);
-
-                            contactFound = true;
-                            break;
-                        } 
-                    }
-                    
-                    if (!contactFound)
-                    {
-                        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
-                    }
+                    SearchContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends, getName, getLastName, contactFound);
                 }
 
                 break;
             case 4: //modify
                 {
-                    Console.WriteLine("Ingrese el nombre del contacto en el cual desea modificar algún dato:");
-                    getName = Console.ReadLine().ToLower();
-
-                    Console.WriteLine("Ingrese el apellido del contacto en el cual desea modificar algún dato:");
-                    getLastName = Console.ReadLine().ToLower();
-
-
-                    foreach (var id in ids)
-                    {
-                        if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
-                        {
-                            contactFound = true;
-
-                            Console.WriteLine("""
-                    Ingrese lo que desea modificar del contacto, las opciones son:
-                    1.Nombre  2.Apellido  3.Dirección  4.Teléfono  5.Email  6.Edad  7.Es mejor amigo?
-                    """);
-                            int selectOptionToModifyContact = Convert.ToInt32(Console.ReadLine());
-
-                            Console.WriteLine("Ingrese el nuevo valor: ");
-                            var newValue = Console.ReadLine();
-
-                            Console.WriteLine("Contacto modificado satisfactoriamente.");
-
-
-                            switch (selectOptionToModifyContact)
-                            {
-                                case 1:
-                                    
-                                    names[id] = newValue;
-
-                                    break;
-
-                                case 2:
-                                    
-                                    lastnames[id] = newValue;
-
-                                    break;
-
-                                case 3:
-                                    
-                                    addresses[id] = newValue;
-
-                                    break;
-
-                                case 4:
-                                    
-                                    telephones[id] = newValue;
-
-                                    break;
-
-                                case 5:
-                                    
-                                    emails[id] = newValue;
-
-                                    break;
-
-                                case 6:
-                                    
-                                    ages[id] = Convert.ToInt32(newValue);
-
-                                    break;
-
-                                case 7:
-                                    
-                                    bestFriends[id] = Convert.ToBoolean(newValue);
-
-                                    break;
-                            }
-                        }
-                    }
-                    
-                    if (!contactFound)
-                    {
-                        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
-                    }
+                    ViewContacts(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
+                    ModifyContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends, getName, getLastName, contactFound);
                 }
                 break;
 
             case 5: //delete
                 {
-                    Console.WriteLine("Ingrese el nombre del contacto que desea eliminar:");
-                    getName = Console.ReadLine().ToLower();
-
-                    Console.WriteLine("Ingrese el apellido del contacto que desea eliminar:");
-                    getLastName = Console.ReadLine().ToLower();
-
-
-                    foreach (var id in ids)
-                    {
-                        if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
-                        {
-
-                            names.Remove(id);
-                            lastnames.Remove(id);
-                            addresses.Remove(id);
-                            telephones.Remove(id);
-                            emails.Remove(id);
-                            ages.Remove(id);
-                            bestFriends.Remove(id);
-
-                            ids.Remove(id);
-
-
-                            Console.WriteLine("Contacto eliminado satisfactoriamente.");
-
-                            contactFound = true;
-                            break;
-                        }
-                    }
-
-                    if (!contactFound)
-                    {
-                        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
-                    }
+                    ViewContacts(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends);
+                    DeleteContact(ids, names, lastnames, addresses, telephones, emails, ages, bestFriends, getName, getLastName, contactFound);
                 }
                 break;
             case 6:
@@ -252,4 +101,175 @@ static void AddContact(List<int> ids, Dictionary<int, string> names, Dictionary<
     emails.Add(id, email);
     ages.Add(id, age);
     bestFriends.Add(id, isBestFriend);
+}
+
+static void ViewContacts(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends)
+{
+    Console.WriteLine($"Nombre          Apellido            Dirección           Telefono            Email           Edad            Es Mejor Amigo?");
+    Console.WriteLine($"____________________________________________________________________________________________________________________________");
+    
+    foreach (var id in ids)
+    {
+        var isBestFriend = bestFriends[id];
+
+        string isBestFriendStr = (isBestFriend == true) ? "Si" : "No";
+        Console.WriteLine($"{names[id]}         {lastnames[id]}         {addresses[id]}         {telephones[id]}            {emails[id]}            {ages[id]}          {isBestFriendStr}");
+    }
+}
+
+static void SearchContact(List<int> ids ,Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends, string getName, string getLastName, bool contactFound)
+{
+
+    Console.WriteLine("Ingrese el nombre del contacto que desea buscar: ");
+    getName = Console.ReadLine().ToLower();
+
+    Console.WriteLine("Ingrese el apellido del contacto que desea buscar: ");
+    getLastName = Console.ReadLine().ToLower();
+
+
+    foreach (var id in ids)
+    {
+
+        if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
+        {
+            
+            Console.WriteLine($"""
+                            Datos del contacto:
+                            Nombre: {names[id]}
+                            Apellido: {lastnames[id]}
+                            Dirección: {addresses[id]}
+                            Teléfono: {telephones[id]}
+                            Email: {emails[id]}
+                            Edad: {ages[id]}
+                            Es mejor amigo?: {bestFriends[id]}
+                            """);
+
+            contactFound = true;
+            break;
+        }
+    }
+
+    if (!contactFound)
+    {
+        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
+    }
+}
+
+static void ModifyContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends, string getName, string getLastName, bool contactFound)
+{
+    Console.WriteLine("Ingrese el nombre del contacto en el cual desea modificar algún dato:");
+    getName = Console.ReadLine().ToLower();
+
+    Console.WriteLine("Ingrese el apellido del contacto en el cual desea modificar algún dato:");
+    getLastName = Console.ReadLine().ToLower();
+
+
+    foreach (var id in ids)
+    {
+        if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
+        {
+            contactFound = true;
+
+            Console.WriteLine("""
+                    Ingrese lo que desea modificar del contacto, las opciones son:
+                    1.Nombre  2.Apellido  3.Dirección  4.Teléfono  5.Email  6.Edad  7.Es mejor amigo?
+                    """);
+            int selectOptionToModifyContact = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el nuevo valor: ");
+            var newValue = Console.ReadLine();
+
+            Console.WriteLine("Contacto modificado satisfactoriamente.");
+
+
+            switch (selectOptionToModifyContact)
+            {
+                case 1:
+
+                    names[id] = newValue;
+
+                    break;
+
+                case 2:
+
+                    lastnames[id] = newValue;
+
+                    break;
+
+                case 3:
+
+                    addresses[id] = newValue;
+
+                    break;
+
+                case 4:
+
+                    telephones[id] = newValue;
+
+                    break;
+
+                case 5:
+
+                    emails[id] = newValue;
+
+                    break;
+
+                case 6:
+
+                    ages[id] = Convert.ToInt32(newValue);
+
+                    break;
+
+                case 7:
+
+                    bestFriends[id] = Convert.ToBoolean(newValue);
+
+                    break;
+            }
+        }
+    }
+
+    if (!contactFound)
+    {
+        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
+    }
+
+}
+
+static void DeleteContact(List<int> ids, Dictionary<int, string> names, Dictionary<int, string> lastnames, Dictionary<int, string> addresses, Dictionary<int, string> telephones, Dictionary<int, string> emails, Dictionary<int, int> ages, Dictionary<int, bool> bestFriends, string getName, string getLastName, bool contactFound)
+{
+    Console.WriteLine("Ingrese el nombre del contacto que desea eliminar:");
+    getName = Console.ReadLine().ToLower();
+
+    Console.WriteLine("Ingrese el apellido del contacto que desea eliminar:");
+    getLastName = Console.ReadLine().ToLower();
+
+
+    foreach (var id in ids)
+    {
+        if (names[id].ToLower().Contains(getName) && lastnames[id].ToLower().Contains(getLastName))
+        {
+
+            names.Remove(id);
+            lastnames.Remove(id);
+            addresses.Remove(id);
+            telephones.Remove(id);
+            emails.Remove(id);
+            ages.Remove(id);
+            bestFriends.Remove(id);
+
+            ids.Remove(id);
+
+            Console.WriteLine("Contacto eliminado satisfactoriamente.");
+
+            contactFound = true;
+            break;
+        }
+    }
+
+    if (!contactFound)
+    {
+        Console.WriteLine("No existe un contacto con ese nombre y apellido.");
+    }
+
 }
